d0571d2100a302e2a3bec2087dc4b031ad7af8fc
tanaka_733 <t.tanaka@grani.jp>
2014-07-03 20:08:17 +0900

    第10回用のコードを追加

--- WebFarmTool/WebFarmTool/Program.cs
+++ WebFarmTool/WebFarmTool/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.Web.Administration;
 
 namespace WebFarmTool
@@ -8,6 +9,7 @@ namespace WebFarmTool
         static void Main(string[] args)
         {
             var program = new Program();
+            // 連載9回のサンプル
             // onlineServer というアドレスのサーバーをWebFarmに追加
             program.AddServer("webfarm-ctrl.cloudapp.net", "onlineServer");
             // onlineServer というアドレスのサーバーをWebFarmにOffline状態で追加
@@ -16,6 +18,11 @@ namespace WebFarmTool
             program.UpdateServerStatus("webfarm-ctrl.cloudapp.net", "webfarm-node1.cloudapp.net", true);
             // webfarm-node2.cloudapp.net サーバーを Offline状態にする
             program.UpdateServerStatus("webfarm-ctrl.cloudapp.net", "webfarm-node2.cloudapp.net", false);
+
+            // 連載10回のサンプル
+            program.UpdateHelathCheck("webfarm-ctrl.cloudapp.net", TimeSpan.FromMinutes(1), TimeSpan.FromSeconds(30), 2);
+            program.UpdateProxySetting("webfarm-ctrl.cloudapp.net", TimeSpan.FromSeconds(10), 512, 8192);
+            program.UpdateClientAffinity("webfarm-ctrl.cloudapp.net", true, "MyARRAffinity");
         }
 
         void AddServer(string webFarmName, string serverName, bool isOnline = true)
@@ -59,6 +66,68 @@ namespace WebFarmTool
                 var server = serversCollection.First(e => e["address"] as string == serverName);
                 server["enabled"] = isOnline;
 
+                manager.CommitChanges();
+            }
+        }
+
+        void UpdateHelathCheck(string webFarmName, TimeSpan interval, TimeSpan timeout, int minServers)
+        {
+            using (var manager = new ServerManager())
+            {
+                // ApplicationHostの設定を取得
+                var appHostConfig = manager.GetApplicationHostConfiguration();
+
+                // WebFarmの設定リストから指定したWebFarm名の要素を取得
+                var webFarmsSection = appHostConfig.GetSection("webFarms");
+                var webFarmSection = webFarmsSection.GetCollection().First(e => e["name"] as string == webFarmName);
+
+                // applicationRequestRouting->healthCheck要素を取得して設定
+                var applicationRequestRouting = webFarmSection.GetChildElement("applicationRequestRouting");
+                var healthCheck = applicationRequestRouting.GetChildElement("healthCheck");
+                healthCheck["interval"] = interval; 
+                healthCheck["timeout"] = timeout;
+                healthCheck["minServers"] = minServers;
+                manager.CommitChanges();
+            }
+        }
+
+        void UpdateProxySetting(string webFarmName, TimeSpan timeout, int minResponseBuffer, int responseBufferLimit)
+        {
+            using (var manager = new ServerManager())
+            {
+                // ApplicationHostの設定を取得
+                var appHostConfig = manager.GetApplicationHostConfiguration();
+
+                // WebFarmの設定リストから指定したWebFarm名の要素を取得
+                var webFarmsSection = appHostConfig.GetSection("webFarms");
+                var webFarmSection = webFarmsSection.GetCollection().First(e => e["name"] as string == webFarmName);
+
+                // applicationRequestRouting->protocol要素を取得して設定
+                var applicationRequestRouting = webFarmSection.GetChildElement("applicationRequestRouting");
+                var protocol = applicationRequestRouting.GetChildElement("protocol");
+                protocol["timeout"] = timeout;
+                protocol["minResponseBuffer"] = minResponseBuffer;
+                protocol["responseBufferLimit"] = responseBufferLimit;
+                manager.CommitChanges();
+            }
+        }
+
+        void UpdateClientAffinity(string webFarmName, bool use, string cookieName)
+        {
+            using (var manager = new ServerManager())
+            {
+                // ApplicationHostの設定を取得
+                var appHostConfig = manager.GetApplicationHostConfiguration();
+
+                // WebFarmの設定リストから指定したWebFarm名の要素を取得
+                var webFarmsSection = appHostConfig.GetSection("webFarms");
+                var webFarmSection = webFarmsSection.GetCollection().First(e => e["name"] as string == webFarmName);
+
+                // applicationRequestRouting->affinity要素を取得して設定
+                var applicationRequestRouting = webFarmSection.GetChildElement("applicationRequestRouting");
+                var affinity = applicationRequestRouting.GetChildElement("affinity");
+                affinity["useCookie"] = use;
+                affinity["cookieName"] = cookieName;
                 manager.CommitChanges();
             }
         }
